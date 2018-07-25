21a39fb1ba0fdf1c1fa761cdef257087aea61fd0
concentratedclorox <40669792+concentratedclorox@users.noreply.github.com>
2018-07-25 14:51:20 +0200

    department dao impl

--- ManagementSystem/src/main/java/com/mert/managementsystem/dao/impl/DepartmentDaoImpl.java
+++ ManagementSystem/src/main/java/com/mert/managementsystem/dao/impl/DepartmentDaoImpl.java
@@ -1,5 +1,43 @@
 package com.mert.managementsystem.dao.impl;
 
-public class DepartmentDaoImpl {
+import java.util.List;
+
+import org.hibernate.Session;
+
+import com.mert.managementsystem.dao.DepartmentDao;
+import com.mert.managementsystem.entities.Department;
+
+public class DepartmentDaoImpl implements DepartmentDao {
+	Session session;
+
+	@Override
+	public void addDepartment(Department dept) {
+		session.persist(dept);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public List<Department> listDepartments() {
+		return session.createCriteria(Department.class).list();
+	}
+
+	@Override
+	public Department getDepartmentById(int id) {
+		return session.get(Department.class, id);
+	}
+
+	@Override
+	public void deleteDepartment(int id) {
+		session.delete(id);
+
+	}
+
+	public Session getSession() {
+		return session;
+	}
+
+	public void setSession(Session session) {
+		this.session = session;
+	}
 
 }
