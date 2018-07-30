e9d8e48b8d19a22b2073c7bb6a810ece10b6c993
concentratedclorox <40669792+concentratedclorox@users.noreply.github.com>
2018-07-30 13:09:57 +0200

    database update.

--- ManagementSystem/Database.sql
+++ ManagementSystem/Database.sql
@@ -1,8 +1,6 @@
-CREATE DATABASE  IF NOT EXISTS `managament_system` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `managament_system`;
 -- MySQL dump 10.13  Distrib 8.0.11, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: managament_system
+-- Host: localhost    Database: managament_system
 -- ------------------------------------------------------
 -- Server version	8.0.11
 
@@ -43,41 +41,43 @@ CREATE TABLE `customer` (
 
 LOCK TABLES `customer` WRITE;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES (1,'Mert','Mert','Ozdogan','23','21321321','123213');
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `departments`
+-- Table structure for table `department`
 --
 
-DROP TABLE IF EXISTS `departments`;
+DROP TABLE IF EXISTS `department`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `departments` (
+CREATE TABLE `department` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `departments`
+-- Dumping data for table `department`
 --
 
-LOCK TABLES `departments` WRITE;
-/*!40000 ALTER TABLE `departments` DISABLE KEYS */;
-INSERT INTO `departments` VALUES (1,'Software'),(2,'Hardware');
-/*!40000 ALTER TABLE `departments` ENABLE KEYS */;
+LOCK TABLES `department` WRITE;
+/*!40000 ALTER TABLE `department` DISABLE KEYS */;
+INSERT INTO `department` VALUES (15,'Backend'),(16,'Frontend'),(17,'UNKNOWN');
+/*!40000 ALTER TABLE `department` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `employeetable`
+-- Table structure for table `employee`
 --
 
-DROP TABLE IF EXISTS `employeetable`;
+DROP TABLE IF EXISTS `employee`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `employeetable` (
+CREATE TABLE `employee` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
   `name` varchar(45) DEFAULT NULL,
@@ -90,18 +90,18 @@ CREATE TABLE `employeetable` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`),
   KEY `deptID_idx` (`deptId`),
-  CONSTRAINT `deptId` FOREIGN KEY (`deptId`) REFERENCES `departments` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `deptId` FOREIGN KEY (`deptId`) REFERENCES `department` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `employeetable`
+-- Dumping data for table `employee`
 --
 
-LOCK TABLES `employeetable` WRITE;
-/*!40000 ALTER TABLE `employeetable` DISABLE KEYS */;
-INSERT INTO `employeetable` VALUES (1,'mert','Mert','Ozdogan','m.mert_ozdogan_@hotmail.com','sanane1','2000',2,'1');
-/*!40000 ALTER TABLE `employeetable` ENABLE KEYS */;
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,'Mert','Mert','Mert','Mert','Mert','Mert',16,'Mert'),(2,'M','M','M','M','M','M',16,'M'),(3,'23','3','4','5','6','7',16,'23'),(63,'e2d',NULL,NULL,NULL,NULL,NULL,16,NULL);
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -115,8 +115,9 @@ CREATE TABLE `goods` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `price` varchar(45) DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,6 +126,7 @@ CREATE TABLE `goods` (
 
 LOCK TABLES `goods` WRITE;
 /*!40000 ALTER TABLE `goods` DISABLE KEYS */;
+INSERT INTO `goods` VALUES (1,'Webcam','31',2);
 /*!40000 ALTER TABLE `goods` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -136,17 +138,14 @@ DROP TABLE IF EXISTS `orders`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `orders` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` varchar(45) NOT NULL,
+  `customerId` int(11) NOT NULL,
   `goodsId` int(11) NOT NULL,
-  `custormerId` int(11) NOT NULL,
-  `amount` int(11) DEFAULT NULL,
-  `date` datetime DEFAULT NULL,
-  `paymenttype` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
+  KEY `customerId_idx` (`customerId`),
   KEY `goodsId_idx` (`goodsId`),
-  KEY `customerId_idx` (`custormerId`),
-  CONSTRAINT `customerId` FOREIGN KEY (`custormerId`) REFERENCES `customer` (`id`),
-  CONSTRAINT `goodsId` FOREIGN KEY (`goodsId`) REFERENCES `goods` (`id`)
+  CONSTRAINT `customerIdfr` FOREIGN KEY (`customerId`) REFERENCES `customer` (`id`),
+  CONSTRAINT `goodsIdfr` FOREIGN KEY (`goodsId`) REFERENCES `goods` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -156,6 +155,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES ('',1,1);
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -168,4 +168,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-16 16:45:03
+-- Dump completed on 2018-07-30 13:08:49
